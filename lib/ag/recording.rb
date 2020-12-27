f3bc4a9b4a4f9b540e289dd11779557cf29f3bb2
sobadon <sbdn@chasoba.net>
2020-12-27 18:45:18 +0900

    agqr HLS

--- lib/ag/recording.rb
+++ lib/ag/recording.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 module Ag
   class Recording
-    AGQR_STREAM_URL = 'rtmp://fms-base2.mitene.ad.jp/agqr/aandg1'
+    AGQR_STREAM_URL = 'https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8'
     CH_NAME = 'ag'
 
     def record(job)
@@ -20,9 +20,15 @@ module Ag
       Main::sleep_until(job.start - 10.seconds)
 
       length = job.length_sec + 60
-      flv_path = Main::file_path_working(CH_NAME, title(job), 'flv')
-      command = "rtmpdump -q -r #{Shellwords.escape(AGQR_STREAM_URL)} --live --stop #{length} -o #{Shellwords.escape(flv_path)}"
-      exit_status, output = Main::shell_exec(command)
+      mp4_path = Main::file_path_working(CH_NAME, title(job), 'mp4')
+      arg = "\
+        -loglevel warning \
+        -y \
+        -i #{Shellwords.escape(AGQR_STREAM_URL)} \
+        -t #{length} \
+        -vcodec none -acodec copy \
+        #{Shellwords.escape(mp4_path)}"
+      exit_status, output = Main::ffmpeg(arg)
       unless exit_status.success?
         Rails.logger.error "rec failed. job:#{job}, exit_status:#{exit_status}, output:#{output}"
         return false
@@ -32,9 +38,7 @@ module Ag
     end
 
     def exec_convert(job)
-      flv_path = Main::file_path_working(CH_NAME, title(job), 'flv')
       mp4_path = Main::file_path_working(CH_NAME, title(job), 'mp4')
-      Main::convert_ffmpeg_to_mp4(flv_path, mp4_path, job)
       Main::move_to_archive_dir(CH_NAME, job.start, mp4_path)
     end
 
